dce82864478a35354ad7ea180f36cde839b090b4
siam9192 <ahsiam999@gmail.com>
2025-05-19 15:23:15 +0600

    Add: Chatroom sidebar ui added

--- src/components/layout/ChatroomLayout.tsx
+++ src/components/layout/ChatroomLayout.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router';
 import Header from '../shared/Header';
+import ChatRoom from '../../pages/ChatRoom';
+import ChatRoomRequest from '../../pages/ChatRoomRequest';
 
 function ChatroomLayout() {
-  const [access, setAccess] = useState(false);
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  const [access, setAccess] = useState(true);
+  return <>{access ? <ChatRoom /> : <ChatRoomRequest />}</>;
 }
 
 export default ChatroomLayout;
